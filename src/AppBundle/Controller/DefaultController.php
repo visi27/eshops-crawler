1ff5430839215b95de2c90063541604d0ccb097a
Evis Bregu <evis.bregu@infotelecom.al>
2017-04-24 12:02:18 +0200

    Parameters test

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,24 +12,12 @@ use Symfony\Component\HttpFoundation\Response;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="homepage2")
      */
     public function indexAction()
     {
-
-        $em = $this->getDoctrine()->getManager();
-        $shop = $em->getRepository('AppBundle:Shop')->findOneBy(['id' => 1]);
-
-        $shopCategories = $em->getRepository('AppBundle:ShopCategory')->findBy(['shop' => 1, 'process' => 1]);
-//
-//        $url = "https://dyqani.shpresa.al/celulare-47-c";
-//
-//        $crawler = new WebCrawler($url);
-//        $products = $crawler->getProducts();
-//        $pages = $crawler->getPages();
-
-        // replace this example code with whatever you need
-        return new Response("<pre>".print_r(count($shopCategories), true)."</pre>");
+        $test = $this->getParameter("shpresa_al");
+        return new Response("<pre>".print_r($test, true)."</pre>");
     }
 
     /**
