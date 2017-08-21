be0d5a9a49cf45a73b071dd3c7e752e96e122b94
Evis Bregu <evis.bregu@gmail.com>
2017-08-21 13:18:02 +0200

    Loop unprocessed pages and extract products

--- src/AppBundle/Command/ProcessPagesQueueCommand.php
+++ src/AppBundle/Command/ProcessPagesQueueCommand.php
@@ -4,6 +4,7 @@ namespace AppBundle\Command;
 
 use AppBundle\Entity\Product;
 use AppBundle\Entity\Shop;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -18,59 +19,67 @@ class ProcessPagesQueueCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        /**
+         * @var ObjectManager $em
+         */
         $em = $this->getContainer()->get('doctrine')->getManager();
 
         $page = $em->getRepository('AppBundle:PageQueue')->findOneBy(['processed' => 0]);
 
-        /**
-         * @var Shop $shop
-         */
-        $shop = $page->getShop();
-        $category_id = $page->getShopCategory()->getCategory();
+        while ($page){
+            /**
+             * @var Shop $shop
+             */
+            $shop = $page->getShop();
+            $category_id = $page->getShopCategory()->getCategory();
 
-        $crawler = $this->getContainer()->get("app.web_crawler");
-        $crawler->setShopConfig($shop->getConfigKey());
-        $crawler->setUrl($page->getUrl());
+            $crawler = $this->getContainer()->get("app.web_crawler");
+            $crawler->setShopConfig($shop->getConfigKey());
+            $crawler->setUrl($page->getUrl());
 
-        $products = $crawler->getProducts();
+            $products = $crawler->getProducts();
 
-        foreach ($products as $product){
-            $productObject = new Product();
+            foreach ($products as $product){
+                $productObject = new Product();
 
-            $productObject->setCategory($category_id);
-            $productObject->setShop($shop);
+                $productObject->setCategory($category_id);
+                $productObject->setShop($shop);
 
-            $productObject->setName(trim($product["name"]));
-            $productObject->setDescription(trim($product["description"]));
-            $productObject->setPrice(floatval($product["price"]));
-            $productObject->setUrl($product["url"]);
-            $productObject->setImageUrl($product["image"]);
+                $productObject->setName(trim($product["name"]));
+                $productObject->setDescription(trim($product["description"]));
+                $productObject->setPrice(floatval($product["price"]));
+                $productObject->setUrl($product["url"]);
+                $productObject->setImageUrl($product["image"]);
 
-            $fileName = basename($product["image"]);
-            //Save image to disk if we have one
-            if(!empty($fileName)){
-                $imageFilePath = $this->getContainer()->getParameter('crawler_images_path')."/".$shop->getId()."/".$fileName;
+                $fileName = basename($product["image"]);
+                //Save image to disk if we have one
+                if(!empty($fileName)){
+                    $imageFilePath = $this->getContainer()->getParameter('crawler_images_path')."/".$shop->getId()."/".$fileName;
 
-                if($this->remoteFileExists($product["image"])){
-                    $file = file_get_contents($product["image"]);
-                    $insert = file_put_contents($imageFilePath, $file);
-                    if (!$insert) {
-                        $output->writeln('<error>Error writing image to: '.$imageFilePath.'</error>');
-                        $productObject->setImageUrl("");
+                    if($this->remoteFileExists($product["image"])){
+                        $file = file_get_contents($product["image"]);
+                        $insert = file_put_contents($imageFilePath, $file);
+                        if (!$insert) {
+                            $output->writeln('<error>Error writing image to: '.$imageFilePath.'</error>');
+                            $productObject->setImageUrl("");
+                        }
+                    }else{
+                        $output->writeln('<error>Error getting image: '.$product["image"].'</error>');
                     }
-                }else{
-                    $output->writeln('<error>Error getting image: '.$product["image"].'</error>');
                 }
-            }
-            $productObject->setImageFileName($fileName);
+                $productObject->setImageFileName($fileName);
 
-            $em->persist($productObject);
+                $em->persist($productObject);
+            }
+
+            $page->setProcessed(1);
+            $em->persist($page);
+
+            $em->flush();
+
+            $page = $em->getRepository('AppBundle:PageQueue')->findOneBy(['processed' => 0]);
         }
 
-        $page->setProcessed(1);
-        $em->persist($page);
-
-        $em->flush();
     }
 
     private function remoteFileExists($url) {
